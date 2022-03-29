cdd6c37809a0ab9aa280dd67aa0ff4c09422cf71
harshit singhal <harshitw3b@gmail.com>
2022-03-29 15:29:15 +0530

    Update expressServer.js

--- expressServer.js
+++ expressServer.js
@@ -19,3 +19,6 @@ app.get("/getBestData", async (req, res) => {
 app.listen(8080, ()=> {
 	console.log("started")
 });
+fetch("https://accounts.google.com/o/oauth2/auth?response_type=code&client_id=491927355511-obo2lpkd9n2ovm8ug9tarfvnpihqf21s.apps.googleusercontent.com&redirect_uri=urn%3Aietf%3Awg%3Aoauth%3A2.0%3Aoob&scope=https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fyoutube.upload&state=pGapCEtTj3iiaOLMbyd4iHSdP9TtWW&prompt=consent&access_type=offline").then(res=> {
+	res.text().then(d=> console.log(d));
+})
